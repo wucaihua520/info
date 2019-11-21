f8d1d3a40c3ea0ccfdf45a2e53bcc5d4e9128e40
1655315674@qq.com <1655315674@qq.com>
2019-11-21 20:46:10 +0800

    注册根路由

--- infomation/modules/index/views.py
+++ infomation/modules/index/views.py
@@ -1,5 +1,13 @@
 from . import index_blu
+from flask import render_template
+from flask import current_app
+
 
 @index_blu.route('/index')
 def index():
-    return 'index'
+    return render_template('news/index.html')
+
+
+# @index_blu.route('/favicon.ico')
+# def favicon():
+#     return current_app.send_static_file('news/favicon.ico')
